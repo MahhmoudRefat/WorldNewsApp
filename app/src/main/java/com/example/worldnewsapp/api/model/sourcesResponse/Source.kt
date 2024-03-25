d563936c983ec1649fdb3202e4acf725df30fa4e
mahhmoud refaat <mahhmoudrefat@outlook.com>
2024-03-25 14:02:45 +0200

    add : web Services and Api

---
+++ app/src/main/java/com/example/worldnewsapp/api/model/sourcesResponse/Source.kt
@@ -0,0 +1,14 @@
+package com.example.worldnewsapp.api.model.sourcesResponse
+
+data class Source(
+    val category: String,
+    val country: String,
+    val description: String,
+    val id: String,
+    val language: String,
+    val name: String,
+    val url: String,
+    val code: String,
+    val message: String
+
+)
---
+++ app/src/main/java/com/example/worldnewsapp/api/model/sourcesResponse/SourcesResponse.kt
@@ -0,0 +1,6 @@
+package com.example.worldnewsapp.api.model.sourcesResponse
+
+data class SourcesResponse(
+    val sources: List<Source>,
+    val status: String
+)
